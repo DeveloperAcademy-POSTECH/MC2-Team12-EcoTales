f68c5e05e229f6a99db963eab33b0739e9f2b5bb
Lia316 <kjk0825@postech.ac.kr>
2022-06-09 22:13:19 +0900

    Feat✨: initial commit (#1)

---
+++ EcoTales/EcoTales/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcoTales
+//
+//  Created by 리아 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcoTales/EcoTales/EcoTalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoTalesApp.swift
+//  EcoTales
+//
+//  Created by 리아 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct EcoTalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
