1d29315b8409bd510e512f78fd2436dcc3e8c766
Lia316 <kjk0825@postech.ac.kr>
2022-06-10 17:10:52 +0900

    Feat✨: add background image (#5)

--- EcoTales/EcoTales/Screens/Map/Views/MapView.swift
+++ EcoTales/EcoTales/Screens/Map/Views/MapView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("map_background_Lia")
+            .resizable()
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
-            .previewInterfaceOrientation(.landscapeLeft)
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
