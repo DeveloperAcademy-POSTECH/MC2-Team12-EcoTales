1cf269dea7c2fcae63d833e64df363d255524b2b
Lia316 <kjk0825@postech.ac.kr>
2022-06-10 17:06:19 +0900

    Feat✨: create new file (#5)

---
+++ EcoTales/EcoTales/Screens/Map/Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  EcoTales
+//
+//  Created by 리아 on 2022/06/10.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
