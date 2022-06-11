c8203c47da6588e454b511bf445586092169a4dc
taek0622 <min4644@gmail.com>
2022-06-11 18:49:37 +0900

    Fix🐞: Fix image Error

--- EcoTales/EcoTales/Global/UIComponent/StoryView.swift
+++ EcoTales/EcoTales/Global/UIComponent/StoryView.swift
@@ -4,43 +4,42 @@
 //
 //  Created by YejiMac on 2022/06/10.
 //
-
 import SwiftUI
 
 struct StoryView: View {
     var body: some View {
-        ZStack{
-            Image("chapter1Background")
+        ZStack {
+            Image("ChapterOneBackground")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
                 .aspectRatio(contentMode: .fill)
-            HStack(spacing: 300){
-                Image("rain")
+            HStack(spacing: 300) {
+                Image("Rain")
                     .resizable()
                     .frame(width: 150.0, height: 240.0)
 
-                Image("mainCharacter")
+                Image("MainCharacter")
                     .resizable()
                     .frame(width: 140.0, height: 250.0)
             }
             .padding(EdgeInsets(top: 50, leading: 50, bottom: 10, trailing: 50))
-            VStack{
+            VStack {
                 Spacer()
-                ZStack(alignment: .topLeading){
+                ZStack(alignment: .topLeading) {
                     Text("내용, 이건 내용압니다, 진짜 무슨 말을 써야할 지 모르겠는데 내용은 아무튼 맞음,aksdfjadksfjkfjadsfasdffa")
                         .frame(width: 650, height: 80, alignment: .topLeading)
                         .lineSpacing(10)
                         .background(
-                            Image("conversationField")
+                            Image("ConversationField")
                                 .resizable()
                                 .frame(width: 750, height: 150)
                         )
                         .padding(.top, 45)
-                    Text("rain")
+                    Text("Rain")
                         .frame(width: 120, height: 30, alignment: .center)
                         .lineSpacing(10)
                         .background(
-                            Image("conversationNameField")
+                            Image("ConversationNameField")
                                 .resizable()
                                 .frame(width: 140, height: 50)
                         )
@@ -54,5 +53,6 @@ struct StoryView: View {
 struct StoryView_Previews: PreviewProvider {
     static var previews: some View {
         StoryView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
---
+++ EcoTales/EcoTales/Screens/Map/Views/Component/File2.swift
@@ -0,0 +1,8 @@
+//
+//  File2.swift
+//  EcoTales
+//
+//  Created by 김민택 on 2022/06/11.
+//
+
+import Foundation
