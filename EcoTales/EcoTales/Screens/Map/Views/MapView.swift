de6844479599a4be44b5e852baa5767cac4f59f1
Lia316 <kjk0825@postech.ac.kr>
2022-06-11 14:03:22 +0900

    Feat✨: stage layout (#5)

--- EcoTales/EcoTales/Screens/Map/Views/MapView.swift
+++ EcoTales/EcoTales/Screens/Map/Views/MapView.swift
@@ -9,10 +9,49 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        Image("map_background_Lia")
-            .resizable()
-            .scaledToFill()
-            .edgesIgnoringSafeArea(.all)
+        ZStack {
+            Image("map_background_Lia")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+
+            Image("path1")
+            Image("path2")
+            Image("path3")
+            Image("path4")
+
+            Image("StageMark")
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    print("1")
+                }
+                .position(x: 110, y: 270)
+
+            Image("StageMark")
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    print("22")
+                }
+                .position(x: 300, y: 320)
+            Image("StageMark")
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    print("333")
+                }
+                .position(x: 410, y: 210)
+            Image("StageMark")
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    print("4444")
+                }
+                .position(x: 550, y: 125)
+            Image("StageMark")
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    print("55555")
+                }
+                .position(x: 720, y: 300)
+        }
     }
 }
 
