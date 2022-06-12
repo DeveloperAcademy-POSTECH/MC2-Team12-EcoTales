88f83f1a02d1adbbcaf26f80510a954eafd4086c
Lia316 <kjk0825@postech.ac.kr>
2022-06-13 08:58:25 +0900

    Feat✨: replace navigation to popup & add child movement (#5)

--- EcoTales/EcoTales/Screens/Map/Views/MapView.swift
+++ EcoTales/EcoTales/Screens/Map/Views/MapView.swift
@@ -8,11 +8,9 @@
 import SwiftUI
 
 struct MapView: View {
-    @State var isComplishedCH1 = false
-    @State var isComplishedCH2 = false
-    @State var isComplishedCH3 = false
-    @State var isComplishedCH4 = false
-    @State var isComplishedCH5 = false
+    @StateObject var chapterViewModel = ChapterViewModel()
+    @State var isPopUp = false
+    @State var selectedChapter = Chapter.one
 
     var body: some View {
         NavigationView {
@@ -22,48 +20,36 @@ struct MapView: View {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
 
-                Image("path1")
-                    .isHidden(!isComplishedCH1)
-                Image("path2")
-                    .isHidden(!isComplishedCH2)
-                Image("path3")
-                    .isHidden(!isComplishedCH3)
-                Image("path4")
-                    .isHidden(!isComplishedCH4)
-
-                NavigationLink(destination: ChapterView(isComplished: self.$isComplishedCH1)) {
-                    Image(isComplishedCH1 ? "StageMark" : "StageMarkGray")
-                        .frame(width: 50, height: 50)
+                ZStack {
+                    Image("path1")
+                        .isHidden(!chapterViewModel.isCompleted(chapter: .one))
+                    Image("path2")
+                        .isHidden(!chapterViewModel.isCompleted(chapter: .two))
+                    Image("path3")
+                        .isHidden(!chapterViewModel.isCompleted(chapter: .three))
+                    Image("path4")
+                        .isHidden(!chapterViewModel.isCompleted(chapter: .four))
                 }
-                .position(x: 110, y: 270)
 
-                NavigationLink(destination: ChapterView(isComplished: self.$isComplishedCH2)) {
-                    Image(isComplishedCH2 ? "StageMark" : "StageMarkGray")
-                        .frame(width: 50, height: 50)
+                ForEach(Chapter.allCases) { chapter in
+                    Button {
+                        isPopUp.toggle()
+                        selectedChapter = chapter
+                    } label: {
+                        Image(chapterViewModel.getStageImage(chapter: chapter))
+                            .frame(width: 50, height: 50)
+                    }
+                    .position(chapterViewModel.getStagePosition(chapter: chapter))
+                    .disabled(chapterViewModel.isDisabled(chapter: chapter))
                 }
-                .position(x: 300, y: 320)
-                .disabled(!isComplishedCH1)
 
-                NavigationLink(destination: ChapterView(isComplished: self.$isComplishedCH3)) {
-                    Image(isComplishedCH3 ? "StageMark" : "StageMarkGray")
-                        .frame(width: 50, height: 50)
-                }
-                .position(x: 410, y: 210)
-                .disabled(!isComplishedCH2)
+                Image("Child")
+                    .position(chapterViewModel.getChildPosition())
 
-                NavigationLink(destination: ChapterView(isComplished: self.$isComplishedCH4)) {
-                    Image(isComplishedCH4 ? "StageMark" : "StageMarkGray")
-                        .frame(width: 50, height: 50)
-                }
-                .position(x: 550, y: 125)
-                .disabled(!isComplishedCH3)
+                ChapterView(chapter: selectedChapter,
+                            chapterViewModel: Binding<ChapterViewModel>.constant(chapterViewModel),
+                            isPop: $isPopUp)
 
-                NavigationLink(destination: ChapterView(isComplished: self.$isComplishedCH5)) {
-                    Image(isComplishedCH5 ? "StageMark" : "StageMarkGray")
-                        .frame(width: 50, height: 50)
-                }
-                .position(x: 720, y: 300)
-                .disabled(!isComplishedCH4)
             }
         }
     }
@@ -77,10 +63,17 @@ struct MapView_Previews: PreviewProvider {
 }
 
 struct ChapterView: View {
-    @Binding var isComplished: Bool
+
+    var chapter: Chapter
+    @Binding var chapterViewModel: ChapterViewModel
+    @Binding var isPop: Bool
+
     var body: some View {
-        Button(isComplished ? "완료 상태👍🏻" : "미완료 상태😅") {
-            isComplished.toggle()
+        if isPop {
+            Button(chapterViewModel.getPopUpMessage(chapter: chapter)) {
+                chapterViewModel.complete(chapter: chapter)
+                isPop = false
+            }
         }
     }
 }
@@ -92,4 +85,75 @@ extension View {
         }
         return AnyView(self)
     }
+}
+
+enum Chapter: Int, CaseIterable {
+    case one = 1, two, three, four, five
+}
+
+extension Chapter: Identifiable {
+    var id: RawValue { rawValue }
+}
+
+class ChapterViewModel: ObservableObject {
+
+    @Published var chapterCompletion: [Chapter: Bool]
+
+    init() {
+        chapterCompletion = Chapter.allCases.reduce(into: [Chapter: Bool]()) {
+            $0[$1] = false
+        }
+    }
+
+    func complete(chapter: Chapter) {
+        chapterCompletion[chapter] = true
+    }
+
+    func isCompleted(chapter: Chapter) -> Bool {
+        return chapterCompletion[chapter]!
+    }
+
+    func getPopUpMessage(chapter: Chapter) -> String {
+        return isCompleted(chapter: chapter) ? "chapter \(chapter.rawValue) 완료" : "chapter \(chapter.rawValue)미완"
+    }
+
+    func isDisabled(chapter: Chapter) -> Bool {
+        if chapter == .one { return false }
+        return !isCompleted(chapter: Chapter(rawValue: chapter.rawValue - 1) ?? .five
+        )
+    }
+
+    func getStagePosition(chapter: Chapter) -> CGPoint {
+        switch chapter {
+        case .one:
+            return CGPoint(x: 110, y: 270)
+        case .two:
+            return CGPoint(x: 300, y: 320)
+        case .three:
+            return CGPoint(x: 410, y: 210)
+        case .four:
+            return CGPoint(x: 550, y: 125)
+        case .five:
+            return CGPoint(x: 720, y: 300)
+        }
+    }
+
+    func getStageImage(chapter: Chapter) -> String {
+        let isCompleted = (chapterCompletion[chapter] ?? false)
+        return isCompleted ? "StageMark" : "StageMarkGray"
+    }
+
+    private func getLastChapter() -> Chapter {
+        let completedChapters = chapterCompletion
+            .filter { !$0.value }
+            .sorted(by: { $0.key.rawValue < $1.key.rawValue })
+        return completedChapters.isEmpty ? Chapter.one : completedChapters[0].key
+    }
+
+    func getChildPosition() -> CGPoint {
+        let chapter = getLastChapter()
+        let position = getStagePosition(chapter: chapter)
+        return CGPoint(x: position.x, y: position.y - 50)
+    }
+
 }
