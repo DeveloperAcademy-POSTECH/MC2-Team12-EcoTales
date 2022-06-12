5174c0d562137dc63eefe314aff8dd1f5adae690
Seik Oh <seikoh@naver.com>
2022-06-12 13:11:40 +0900

    Wip⚙: newly made project file (#28) Merge conflict kept occuring so newly cloned project From this point on, EpilogueThree is made on top of the newly merged project

--- EcoTales/EcoTales/Screens/Epilogue/ViewModels/EpilogueThree.swift
+++ EcoTales/EcoTales/Screens/Epilogue/ViewModels/EpilogueThree.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct EpilogueThree: View {
+    let images = ["chapterThree_cleanBackground", "chapterTwo_background", "chapterOne_cleanBackground"]
+    let imageChangeTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    @State private var currentImageIndex = 0
     var body: some View {
         ZStack {
-            Image("backgroundPolluted")
+            Image(images[currentImageIndex])
                 .resizable()
                 .ignoresSafeArea()
-            VStack {
-                Image("woodenPanel")
-                Spacer()
-                ObjectsToFind()
-            }
-            LocatingHiddenObjView()
+                .transition(AnyTransition.opacity.animation(.easeInOut(duration: 1.0)))
+                .onReceive(imageChangeTimer) { _ in
+                    withAnimation(.easeInOut(duration: 1.0)) {
+                        self.currentImageIndex = (self.currentImageIndex + 1) % self.images.count
+                    }
+                }
         }
     }
 }
@@ -26,5 +29,6 @@ struct EpilogueThree: View {
 struct EpilogueThree_Previews: PreviewProvider {
     static var previews: some View {
         EpilogueThree()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
