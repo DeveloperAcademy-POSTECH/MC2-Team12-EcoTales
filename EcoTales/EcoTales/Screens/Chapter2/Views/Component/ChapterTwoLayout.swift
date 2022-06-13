a808723b7bc12ba99a961fdfa3f3063cb9c1c465
Seik Oh <seikoh@naver.com>
2022-06-13 15:19:05 +0900

    Refactor🔨: Add new ChapterTwoLayout file (#47)

---
+++ EcoTales/EcoTales/Screens/Chapter2/Views/Component/ChapterTwoLayout.swift
@@ -0,0 +1,20 @@
+//
+//  ChapterTwoLayout.swift
+//  EcoTales
+//
+//  Created by Seik Oh on 2022/06/13.
+//
+
+import SwiftUI
+
+struct ChapterTwoLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChapterTwoLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        ChapterTwoLayout()
+    }
+}
