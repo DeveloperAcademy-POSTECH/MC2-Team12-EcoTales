130b2b2d8499bf6b73b20c5bd82c100f25500843
Lia316 <kjk0825@postech.ac.kr>
2022-06-14 21:11:16 +0900

    Chore🧹: change image file name (#53)

--- EcoTales/EcoTales/Global/Literal/ImageLiteral.swift
+++ EcoTales/EcoTales/Global/Literal/ImageLiteral.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 enum ImageLiteral {
-    static var mapBackground = "map_background_Lia"
+    static var mapBackground = "map_background"
     static var path1to2 = "path1"
     static var path2to3 = "path2"
     static var path3to4 = "path3"
     static var path4to5 = "path4"
-    static var child = "Child"
-    static var activeStageMark = "StageMark"
-    static var inactiveStageMark = "StageMarkGray"
+    static var child = "child"
+    static var activeStageMark = "stageMark"
+    static var inactiveStageMark = "stageMarkGray"
 }
