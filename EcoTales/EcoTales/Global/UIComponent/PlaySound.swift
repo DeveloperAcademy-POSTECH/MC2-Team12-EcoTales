aef428248157e341be861dc636abc586d5a0eb3e
sherry <yeahaluu@gmail.com>
2022-06-14 11:42:50 +0900

    Chore🧹: Remove remark

--- EcoTales/EcoTales/Global/UIComponent/PlaySound.swift
+++ EcoTales/EcoTales/Global/UIComponent/PlaySound.swift
@@ -3,7 +3,7 @@
 //  EcoTales
 //
 //  Created by YejiMac on 2022/06/14.
-//  https://www.hackingwithswift.com/forums/swiftui/playing-sound/4921
+//
 
 import AVFoundation
 
@@ -13,7 +13,6 @@ func playSounds(_ soundFileName : String) {
     guard let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: nil) else {
         fatalError("Unable to find \(soundFileName) in bundle")
     }
-
     do {
         audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
     } catch {
