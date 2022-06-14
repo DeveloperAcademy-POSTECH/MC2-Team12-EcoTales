ad8194d52e692c7d94d8426850b7c153b781a007
sherry <yeahaluu@gmail.com>
2022-06-14 15:04:12 +0900

    Feat✨: Check if the trash can button is correct or incorrect

---
+++ EcoTales/EcoTales/Screens/Chapter2/Views/TrashButtonTestView.swift
@@ -0,0 +1,14 @@
+//
+//  TrashButtonTestView.swift
+//  EcoTales
+//
+//  Created by YejiMac on 2022/06/14.
+//
+
+import SwiftUI
+
+struct TrashButtonTestView: View {
+    var body: some View {
+        Text("버튼을 통해 맞다 틀리다를 보내보자")
+    }
+}
