bd03f33a84bc97f11c0fe4900743d2c57401cd73
sherry <yeahaluu@gmail.com>
2022-06-15 22:29:11 +0900

    Wip⚙: add trash model

--- EcoTales/EcoTales/Screens/Chapter2/Views/TrashButtonTestView.swift
+++ EcoTales/EcoTales/Screens/Chapter2/Views/TrashButtonTestView.swift
@@ -5,10 +5,16 @@
 //  Created by YejiMac on 2022/06/14.
 //
 
-import SwiftUI
+// 이미지쓰레기가 쓰레기통버튼과 맞는지 확인해서
+// 쓰레기 맞으면 다음 쓰레기로 넘기기
 
-struct TrashButtonTestView: View {
-    var body: some View {
-        Text("버튼을 통해 맞다 틀리다를 보내보자")
-    }
+
+enum Garbage: String, CaseIterable {
+    case general
+    case vinyl
+    case can
+    case plastic
+    case paper
+    case glass
 }
+
