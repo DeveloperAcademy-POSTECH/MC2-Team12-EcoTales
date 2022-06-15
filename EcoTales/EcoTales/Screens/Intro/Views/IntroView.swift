c8983713da608ccf6484028405793132c36e2cda
taek0622 <min4644@gmail.com>
2022-06-15 23:31:35 +0900

    Refactor🔨: Combine Intro View (#59)
    IntroView, IntroOneView, IntroTwoView, IntroThreeView, IntroFourView의 5개의 뷰로 나누어져 있던 Intro View를 IntroView 한 개로 통합

--- EcoTales/EcoTales/Screens/Intro/Views/IntroView.swift
+++ EcoTales/EcoTales/Screens/Intro/Views/IntroView.swift
@@ -7,20 +7,99 @@
 
 import SwiftUI
 
+extension Image: ViewModifier {
+    func backgroundImage() -> some View {
+        self
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+func selectIntroBackground(introNumber: Int) -> String {
+    switch introNumber {
+    case 1:
+        return "intro1_background"
+    case 2:
+        return "intro2_background"
+    case 3:
+        return "intro3_background"
+    case 4:
+        return "intro4_background"
+    default:
+        return "intro1_background"
+    }
+}
+
+func selectIntroStory(introNumber: Int) -> [String] {
+    switch introNumber {
+    case 1:
+        return StoryData().introOne
+    case 2:
+        return StoryData().introTwo
+    case 3:
+        return StoryData().introThree
+    case 4:
+        return StoryData().introFour
+    default:
+        return ["Error!", "Error!", "Error!"]
+    }
+}
+
 struct IntroView: View {
     @State private var introNumber = 1
+    @State private var introIndex = 0
+    let screenWidth = UIScreen.main.bounds.size.width
+
     var body: some View {
-        switch introNumber {
-        case 1:
-            IntroOneView(introNumber: self.$introNumber)
-        case 2:
-            IntroTwoView(introNumber: self.$introNumber)
-        case 3:
-            IntroThreeView(introNumber: self.$introNumber)
-        case 4:
-            IntroFourView(introNumber: self.$introNumber)
-        default:
-            MapBackground()
+        ZStack {
+            Image(selectIntroBackground(introNumber: self.introNumber))
+                .backgroundImage()
+            HStack {
+                if introNumber == 3 {
+                    Spacer()
+                        .frame(width: screenWidth / 2)
+                }
+                VStack(alignment: introNumber == 4 ? .center : .leading, spacing: 10) {
+                    Text(selectIntroStory(introNumber: self.introNumber)[introIndex / 3 * 3 + 0])
+                    HStack {
+                        if introNumber != 4 {
+                            Spacer()
+                        }
+                        Text("다음")
+                    }
+                    .opacity(introIndex.isMultiple(of: 3) ? 1 : 0)
+                    Text(selectIntroStory(introNumber: self.introNumber)[introIndex / 3 * 3 + 1])
+                        .opacity(introIndex % 3 >= 1 ? 1 : 0)
+                    HStack {
+                        if introNumber != 4 {
+                            Spacer()
+                        }
+                        Text("다음")
+                    }
+                    .opacity(introIndex % 3 == 1 ? 1 : 0)
+                    Text(selectIntroStory(introNumber: self.introNumber)[introIndex / 3 * 3 + 2])
+                        .opacity(introIndex % 3 >= 2 ? 1 : 0)
+                    HStack {
+                        if introNumber != 4 {
+                            Spacer()
+                        }
+                        Text("다음")
+                    }
+                    .opacity(introIndex % 3 == 2 ? 1 : 0)
+                }
+                if introNumber == 1 || introNumber == 2 {
+                    Spacer()
+                        .frame(width: screenWidth / 2)
+                }
+            }
+        }
+        .onTapGesture {
+            if introIndex == selectIntroStory(introNumber: self.introNumber).count - 1 {
+                introNumber += 1
+                introIndex = 0
+            } else {
+                introIndex += 1
+            }
         }
     }
 }
