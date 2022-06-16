fdbca73f6b128fb8dd2830480b8f834c74c402c1
taek0622 <min4644@gmail.com>
2022-06-16 16:56:50 +0900

    Refactor🔨: Add ImageLiteral Contents

--- EcoTales/EcoTales/Global/Literal/ImageLiteral.swift
+++ EcoTales/EcoTales/Global/Literal/ImageLiteral.swift
@@ -8,12 +8,52 @@
 import Foundation
 
 enum ImageLiteral {
+    // MARK: - Map Image
     static var mapBackground = "map_background"
     static var path1to2 = "map_path1"
     static var path2to3 = "map_path2"
     static var path3to4 = "map_path3"
     static var path4to5 = "map_path4"
-    static var child = "character_child"
     static var activeStageMark = "map_stageMark"
     static var inactiveStageMark = "map_stageMarkGray"
+    // MARK: - Chapter Background Image
+    static var chapter1CleanBackground = "chapter1_cleanBackground"
+    static var chapter1PollutedBackground = "chapter1_pollutedBackground"
+    static var chapter1TurtleDialog = "chapter1_turtleDialog"
+    static var chapter1TurtleHeadCircle = "chapter1_turtleHeadCircle"
+    static var chapter2Background = "chapter2_background"
+    static var chapter3CleanBackground = "chapter3_cleanBackground"
+    static var chapter3Pollutedbackground = "chapter3_pollutedBackground"
+    static var chapter4Background = "chapter4_background"
+    // MARK: - Character Image
+    static var child = "character_child"
+    static var meenu = "character_meenu"
+    static var mole = "character_mole"
+    static var oz = "character_oz"
+    static var rain = "character_rain"
+    static var redPanda = "character_redPanda"
+    static var redPandas = "character_redPandas"
+    static var sherry = "character_sherry"
+    
+    static var epilogueValley = "epilogue_valley"
+    static var woodenPanel = "game_woodenPanel"
+    
+    static var dialogField = "global_dialogField"
+    static var nameField = "global_nameField"
+    // MARK: - Hidden Game Object Image
+    static var can = "hidden_can"
+    static var cutlery = "hidden_cutlery"
+    static var galssBottle = "hidden_glassBottle"
+    static var plasticBag = "hidden_plasticBag"
+    static var plasticBottle = "hidden_plasticBottle"
+    static var plasticCup = "hidden_plasticCup"
+    static var smoking = "hidden_smoking"
+    static var snack = "hidden_snack"
+    static var straw = "hidden_straw"
+    static var toothBrush = "hidden_toothBrush"
+    // MARK: - Intro Image
+    static var intro1Background = "intro1_background"
+    static var intro2Background = "intro2_background"
+    static var intro3Background = "intro3_background"
+    static var intro4Background = "intro4_background"
 }
