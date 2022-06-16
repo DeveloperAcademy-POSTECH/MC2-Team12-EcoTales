852555d080015d6018ff3beb17e7d8877ee18f71
sherry <yeahaluu@gmail.com>
2022-06-16 23:16:56 +0900

    Wip⚙: Fix Recycle Object Data

--- EcoTales/EcoTales/Models/RecycleObjectData.swift
+++ EcoTales/EcoTales/Models/RecycleObjectData.swift
@@ -5,13 +5,34 @@
 //  Created by YejiMac on 2022/06/15.
 //
 
-import Foundation
 
-enum Garbage: Int {
-    case general = 0
-    case vinyl
-    case can
-    case plastic
-    case paper
-    case glass
+enum ImageRecycleObject {
+    static var litter = "recycle_litter"
+    static var egg = "recycle_egg"
+    static var bag = "recycle_plasticBag"
+    static var snack = "recycle_snack"
+    static var coke = "recycle_coke"
+    static var drink = "recycle_drinkCan"
+    static var straw = "recycle_straw"
+    static var water = "recycle_water"
+    static var box = "recycle_paperBox"
+    static var newspaper = "recycle_newspaper"
+    static var cup = "recycle_glassCup"
+    static var bottle = "recycle_glassBottle"
+}
+
+enum RecycleObject {
+    case litter
+    case egg
+    case bag
+    case snack
+    case coke
+    case drink
+    case straw
+    case water
+    case box
+    case newspaper
+    case cup
+    case bottle
+    
 }
