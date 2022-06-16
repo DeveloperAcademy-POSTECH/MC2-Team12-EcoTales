9a2029199303df9524ce8b65c6ccdf44223f27e9
Seik Oh <seikoh@naver.com>
2022-06-16 16:38:16 +0900

    Chore🧹: edit lines for readability (#77)

--- EcoTales/EcoTales/Screens/Chapter4/PandaGameView.swift
+++ EcoTales/EcoTales/Screens/Chapter4/PandaGameView.swift
@@ -8,10 +8,11 @@ import AVFoundation
 import SwiftUI
 
 struct PandaGameView: View {
+
     @State private var showGameClear = false
-    @State private var isGameFinished = false
     @State private var isShowingPopup = false
     @State var audioPlayer: AVAudioPlayer!
+
     var body: some View {
         ZStack {
             Image("chapterFour-gameBackground")
@@ -20,6 +21,7 @@ struct PandaGameView: View {
                 .onTapGesture(  perform: {
                     SoundManager.playSounds("mixkit-player-jumping-in-a-video-game-2043.wav")
                 })
+
             VStack {
                 HStack {
                     Button(action: {
@@ -32,10 +34,10 @@ struct PandaGameView: View {
                 .padding(.leading, 10)
                 Spacer()
             }
+
             Group {
                 Button(action: {
                     showGameClear.toggle()
-                    isGameFinished.toggle()
                 })
                 {
                     Image("character_meenuDoctor")
@@ -45,6 +47,7 @@ struct PandaGameView: View {
                 }
                 .position(x: 520, y: 110)
             }
+
             if showGameClear {
                 Color.black.opacity(0.4)
                     .ignoresSafeArea()
@@ -54,7 +57,11 @@ struct PandaGameView: View {
                     .onAppear(perform: {
                         SoundManager.playSounds("correctAnswer.mp3")
                     })
+                    .onTapGesture(  perform: {
+                        // todo: go to next view
+                    })
             }
+
             if isShowingPopup {
                 StoryPopup(isShowingPopup: $isShowingPopup)
                     .ignoresSafeArea()
