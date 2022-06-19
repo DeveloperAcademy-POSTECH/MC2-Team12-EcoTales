72a0f0b6e797805daa163e4d56c3dc75488c8a44
taek0622 <min4644@gmail.com>
2022-06-19 16:51:01 +0900

    Chore🧹: Replace function for introView

--- EcoTales/EcoTales/Screens/Intro/Views/IntroView.swift
+++ EcoTales/EcoTales/Screens/Intro/Views/IntroView.swift
@@ -7,37 +7,8 @@
 
 import SwiftUI
 
-func selectIntroBackground(introNumber: Int) -> String {
-    switch introNumber {
-    case 1:
-        return ImageLiteral.intro1Background
-    case 2:
-        return ImageLiteral.intro2Background
-    case 3:
-        return ImageLiteral.intro3Background
-    case 4:
-        return ImageLiteral.intro4Background
-    default:
-        return ImageLiteral.intro1Background
-    }
-}
-
-func selectIntroStory(introNumber: Int) -> [String] {
-    switch introNumber {
-    case 1:
-        return StoryData().introOne
-    case 2:
-        return StoryData().introTwo
-    case 3:
-        return StoryData().introThree
-    case 4:
-        return StoryData().introFour
-    default:
-        return ["Error!", "Error!", "Error!"]
-    }
-}
-
 struct IntroView: View {
+    @EnvironmentObject var chapterProgress: ChapterProgress
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State private var introNumber = 1
     @State private var introStoryIndex = 0
@@ -46,41 +17,51 @@ struct IntroView: View {
 
     var body: some View {
         ZStack {
-            Image(selectIntroBackground(introNumber: self.introNumber))
+            Image(introBackground())
                 .backgroundImage()
             HStack {
                 if introNumber == 3 {
                     Spacer()
                 }
                 VStack(alignment: introNumber != 4 ? .leading : .center) {
-                    Text(selectIntroStory(introNumber: self.introNumber)[introStoryIndex / 3 * 3 + 0])
+                    Text(introStoryContent(position: 0))
+                        .font(.seoulHangangM.story)
                     HStack {
                         Spacer()
                         Text("다음")
+                            .font(.seoulHangangM.story)
                     }
                     .opacity(introStoryIndex.isMultiple(of: 3) ? 1 : 0)
-                    Text(selectIntroStory(introNumber: self.introNumber)[introStoryIndex / 3 * 3 + 1])
+                    Text(introStoryContent(position: 1))
+                        .font(.seoulHangangM.story)
                         .opacity(introStoryIndex % 3 >= 1 ? 1 : 0)
                     HStack {
                         Spacer()
                         Text("다음")
+                            .font(.seoulHangangM.story)
                     }
                     .opacity(introStoryIndex % 3 == 1 ? 1 : 0)
-                    Text(selectIntroStory(introNumber: self.introNumber)[introStoryIndex / 3 * 3 + 2])
+                    Text(introStoryContent(position: 2))
+                        .font(.seoulHangangM.story)
                         .opacity(introStoryIndex % 3 >= 2 ? 1 : 0)
                     HStack {
                         Spacer()
                         Text("다음")
+                            .font(.seoulHangangM.story)
                     }
                     .opacity(introStoryIndex % 3 == 2 ? 1 : 0)
                 }
                 .frame(width: screenWidth * 0.45)
+                if introNumber == 1 || introNumber == 2 {
+                    Spacer()
+                }
             }
         }
         .onTapGesture {
-            if introStoryIndex == selectIntroStory(introNumber: self.introNumber).count - 1 {
+            if introStoryIndex == introStory().count - 1 {
                 if introNumber == 4 {
                     isIntroSeen = true
+//                    chapterProgress.completionStatus[.zero] = true
                     presentationMode.wrappedValue.dismiss()
                 } else {
                     introNumber += 1
@@ -90,5 +71,39 @@ struct IntroView: View {
                 introStoryIndex += 1
             }
         }
+    }
+
+    func introBackground() -> String {
+        switch introNumber {
+        case 1:
+            return ImageLiteral.intro1Background
+        case 2:
+            return ImageLiteral.intro2Background
+        case 3:
+            return ImageLiteral.intro3Background
+        case 4:
+            return ImageLiteral.intro4Background
+        default:
+            return ImageLiteral.intro1Background
+        }
+    }
+
+    func introStory() -> [String] {
+        switch introNumber {
+        case 1:
+            return StoryData().introOne
+        case 2:
+            return StoryData().introTwo
+        case 3:
+            return StoryData().introThree
+        case 4:
+            return StoryData().introFour
+        default:
+            return ["Error!", "Error!", "Error!"]
+        }
+    }
+    
+    func introStoryContent(position: Int) -> String {
+        return introStory()[introStoryIndex / 3 * 3 + position]
     }
 }
