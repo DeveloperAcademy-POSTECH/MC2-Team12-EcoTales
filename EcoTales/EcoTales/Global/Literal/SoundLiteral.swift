e669b8ed88aeadeffbd3c924c713212c7662f364
Seik Oh <seikoh@naver.com>
2022-06-19 20:24:57 +0900

    Chore🧹: Add SoundLiteral

---
+++ EcoTales/EcoTales/Global/Literal/SoundLiteral.swift
@@ -0,0 +1,13 @@
+//
+//  SoundLiteral.swift
+//  EcoTales
+//
+//  Created by Seik Oh on 2022/06/19.
+//
+
+import Foundation
+
+enum SoundLiteral {
+    static var SoundForWrong = "mixkit-player-jumping-in-a-video-game-2043"
+    static var SoundForRight  = "correctAnswer"
+}
